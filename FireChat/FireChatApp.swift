1afbbe368664bdc6cfd26c486c9c8e7fe266d105
benuhdrill <bgmach2023@fau.edu>
2024-11-04 13:35:53 -0500

    Initial Commit

---
+++ FireChat/FireChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireChatApp.swift
+//  FireChat
+//
+//  Created by Ben Gmach on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct FireChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
