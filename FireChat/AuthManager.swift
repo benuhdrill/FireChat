dc836322130f8e998bca39fcd080012175adebff
benuhdrill <bgmach2023@fau.edu>
2024-11-04 23:30:11 -0500

    First commit

---
+++ FireChat/AuthManager.swift
@@ -0,0 +1,18 @@
+//
+//  AuthManager.swift
+//  FireChat
+//
+//  Created by Ben Gmach on 11/4/24.
+//
+
+import SwiftUI
+
+struct AuthManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthManager()
+}
---
+++ FireChat/MessageManager.swift
@@ -0,0 +1,8 @@
+//
+//  MessageManager.swift
+//  FireChat
+//
+//  Created by Ben Gmach on 11/4/24.
+//
+
+import Foundation
